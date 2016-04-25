953d938db5b92aadb8d016e4534d4d303c2923e1
Lennart Ekberg <lennart.ekberg71@gmail.com>
2016-04-25 09:42:32 +0200

    Uppdaterat populateList()

--- src/main/java/com/idnoll/controllers/QuizController.java
+++ src/main/java/com/idnoll/controllers/QuizController.java
@@ -21,6 +21,30 @@ public class QuizController {
 	
 	public void populateList(){
 		//populera lista med questionModel objekt.
+		QuestionModel questionOne = new QuestionModel();
+		questionOne.setQuestion("En mycket känd NBA-spelare avslutade sin karriär nyligen. Han heter Bryant i efternamn, men vad heter han i förnamn?");
+		questionOne.setCorrectAnswer("Kobe");
+		questionOne.setWrongAnswer("Code");
+		questionOne.setWrongAnswer2("Frode");
+		questionOne.setTopic("Sport");
+		questions.add(questionOne);
+		
+		QuestionModel questionTwo = new QuestionModel();
+		questionTwo.setQuestion("I september startar en hockeyturnering med världens bästa herrspelare. Vad heter turneringen?");
+		questionTwo.setCorrectAnswer("World Cup of Hockey");
+		questionTwo.setWrongAnswer("Stanley Cup");
+		questionTwo.setWrongAnswer2("Connie Smythe Memorial Cup");
+		questionTwo.setTopic("Sport");
+		questions.add(questionTwo);
+		
+		QuestionModel questionThree = new QuestionModel();
+		questionThree.setQuestion("Var spelas fotbolls-EM 2016?");
+		questionThree.setCorrectAnswer("Frankrike");
+		questionThree.setWrongAnswer("Spanien");
+		questionThree.setWrongAnswer2("Italien");
+		questionThree.setTopic("Sport");
+		questions.add(questionThree);
+		
 	}
 	
 	public void printQuestion(QuestionModel question){
