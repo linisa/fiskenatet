3e5e2d80a7c25d48e3ee7cc94889e2ab33a6dd39
unknown <marcinretek@hotmail.com>
2016-04-25 14:54:53 +0200

    fixed shit i add funktion

--- src/main/java/com/example/fiskenatet/scripts/addProduct.js
+++ src/main/java/com/example/fiskenatet/scripts/addProduct.js
@@ -37,7 +37,7 @@ $(document).ready(function () {
             "shortDescription": $('#txtShortDes').val(),
             "longDescription": $('#txtLongDes').val(),
             "startPrice": $('#txtStartPrice').val(),
-            "buyNowPrice": $('#txtBuyNow').val(),
+            "buyNowPrice": $('#txtBuyNow').val()
             /*"startDate": startDate*/
         });
         console.log(product);
