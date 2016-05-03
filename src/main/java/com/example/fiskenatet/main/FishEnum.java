79bc59663e188520d6fcbd0176dbece7559a8b8c
ArmanRaftari <Raftari1@hotmail.com>
2016-05-03 14:29:12 +0200

    enum och categorymeny

---
+++ src/main/java/com/example/fiskenatet/main/FishEnum.java
@@ -0,0 +1,10 @@
+package com.example.fiskenatet.main;
+
+/**
+ * Created by Arman on 2016-05-03.
+ */
+public enum FishEnum {
+    TORSK, LAX, KOLJA, MAKRILL,RÖDSPOTTA, GRÅSEJ, SILL,
+    VITLING,SKALDJUR,ÖVRIGT
+}
+
