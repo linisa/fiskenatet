304446a6ca84987c089e2f9b22278701eb3db53d
onordin <nordin.oscar@gmail.com>
2016-05-06 14:12:21 +0200

    changed rating, added mail functions

--- src/main/java/com/example/fiskenatet/Application.java
+++ src/main/java/com/example/fiskenatet/Application.java
@@ -1,24 +1,20 @@
 package com.example.fiskenatet;
 
-import com.example.fiskenatet.main.SendEmail;
-import com.example.fiskenatet.main.UserRating;
-import com.example.fiskenatet.models.UserModel;
+import com.example.fiskenatet.main.MailHandler;
+import com.example.fiskenatet.services.ProductService;
 import com.example.fiskenatet.services.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Hello world!
- *
- */
+
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(Application.class, args);
-		SendEmail sendEmail = new SendEmail();
-		sendEmail.pung();
+
+
 	}
 
 }
