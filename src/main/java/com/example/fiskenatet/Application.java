03352c5b57ee0afea9bf65ef26136dd800356723
onordin <nordin.oscar@gmail.com>
2016-05-16 11:15:51 +0200

    Added logging-java-class

--- src/main/java/com/example/fiskenatet/Application.java
+++ src/main/java/com/example/fiskenatet/Application.java
@@ -1,20 +1,43 @@
 package com.example.fiskenatet;
 
+import com.example.fiskenatet.logging.Logging;
 import com.example.fiskenatet.main.MailHandler;
 import com.example.fiskenatet.services.ProductService;
 import com.example.fiskenatet.services.UserService;
+import org.apache.log4j.PropertyConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.*;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 
 @SpringBootApplication
 public class Application {
 
+
+
+
+
 	public static void main(String[] args) {
 
+
+		Logging logging = new Logging();
+		Logger log = logging.createLog();
+
+		log.info("Going to start the whole application");
 		SpringApplication.run(Application.class, args);
+
+		System.out.println("Hello Sweden!!!!!");
+
+		log.info("Hello sweden har skrivits ut..");
+		log.info("Här kommer en ny rad");
 
 
 	}
+
+
 
 }
---
+++ src/main/java/com/example/fiskenatet/logging/Logging.java
@@ -0,0 +1,27 @@
+package com.example.fiskenatet.logging;
+
+import com.example.fiskenatet.Application;
+
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+/**
+ * Created by nordi_000 on 2016-05-16.
+ */
+public class Logging {
+
+    private static Logger log = Logger.getLogger(Application.class.getName());
+
+    public Logger createLog() {
+        try {
+            FileHandler fileHandler = new FileHandler("snallafunkanu.txt");
+            fileHandler.setFormatter(new SimpleFormatter());
+            log.addHandler(fileHandler);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return log;
+    }
+}
