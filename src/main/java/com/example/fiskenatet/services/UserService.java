c198a9b8e4537dda80654c50b6e30b76fbb77a03
linisa <linisa89@hotmail.com>
2016-05-17 10:29:53 +0200

    .

--- src/main/java/com/example/fiskenatet/services/UserService.java
+++ src/main/java/com/example/fiskenatet/services/UserService.java
@@ -116,7 +116,6 @@ public class UserService {
             }if(compareUser.getEmail().equals(userModel.getEmail())){
                 checkUser = "Mail already registered";
             }
-            break;
         }
 
         if(userModel.getFirstName().equals("")||userModel.getFirstName().equals(" ")){
