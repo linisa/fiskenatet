467ebab6be64c50115e833b43c3b0f530842de65
unknown <marcinretek@hotmail.com>
2016-05-24 16:14:32 +0200

    felhantering

--- src/main/java/com/example/fiskenatet/scripts/userProfile.js
+++ src/main/java/com/example/fiskenatet/scripts/userProfile.js
@@ -17,18 +17,18 @@ function CheckIfLoggedIn() {
 
 $(document).ready(function () {
 //TIMECHECKER
-    var hasMovedAuctions =false;
     window.setInterval(function(){
         var currentTime = new Date();
-        if(currentTime.getHours()==16 && currentTime.getMinutes()==00 && hasMovedAuctions==false){
-            console.log("Dagens auktioner avslutade!");
-            hasMovedAuctions=true;
-            moveExpiredAuctions();
+        if(currentTime.getHours()==16 && currentTime.getMinutes()==00){
+            setTimeout(function () {
+                moveExpiredAuctions();
+            }, 60000);
         }
     }, 5000);
 
 
     function moveExpiredAuctions() {
+        console.log("Dagens auktioner avslutade!");
         $.ajax({
             type: 'GET',
             contentType: 'application/json',
@@ -100,16 +100,6 @@ $(document).ready(function () {
             }
         });
     }
-
-    $(document).on("click", "#lnkSetProductAsSold", function () {
-        var currentProductID = $(this).data("value");
-        getProductById(currentProductID, function (currentProduct) {
-            setBuyerRating(currentProduct);
-            moveSoldProductToHistory(currentProduct);
-        });
-    });
-
-
 
 
     $(document).on("click", "#lnkLogOut", function () {
@@ -403,6 +393,7 @@ $(document).ready(function () {
     });
 
     function getProductById(currentProductId, callBack) {
+        console.log("i getProductById() " + currentProductId);
         $.ajax({
             type: 'GET',
             contentType: 'application/json',
